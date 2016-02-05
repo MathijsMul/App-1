3520960c36935ffeae6a5bf586ab15f2435a3ae6
practicum <practicum@wcw-staff-145-18-169-153.wireless.uva.nl>
2016-02-05 14:55:05 +0100

    Initial Commit

---
+++ App 1 - Mathijs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App 1 - Mathijs
+//
+//  Created by practicum on 05/02/16.
+//  Copyright © 2016 practicum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
